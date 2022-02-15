343a15bd1722ddc3e226ea46a8c0784a51288c9d
Dzyfhuba <hafidz21ub@gmail.com>
2022-02-15 21:41:11 +0700

    add main & desc

---
+++ script.js
@@ -0,0 +1,17 @@
+// var loader = setInterval(function() {
+//     if (document.readyState !== "complete") return;
+//     clearInterval(loader);
+//     window.onscroll = function() { myFunction() };
+
+//     var navbar = document.querySelector("#navbar");
+//     var sticky = navbar.offsetTop;
+
+//     function myFunction() {
+//         if (window.pageYOffset >= sticky) {
+//             navbar.classList.add("sticky")
+//         } else {
+//             navbar.classList.remove("sticky");
+//         }
+//     }
+//     // document.write("Document loaded successful!");
+// }, 1000);

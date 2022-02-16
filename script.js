ed5237dea66678d9ad40eb90d783adfacb45c01a
Dzyfhuba <hafidz21ub@gmail.com>
2022-02-17 01:36:34 +0700

    add desktop mode

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-// var loader = setInterval(function() {
-//     if (document.readyState !== "complete") return;
-//     clearInterval(loader);
-//     window.onscroll = function() { myFunction() };
+var loader = setInterval(function() {
+    if (document.readyState !== "complete") return;
+    clearInterval(loader);
 
-//     var navbar = document.querySelector("#navbar");
-//     var sticky = navbar.offsetTop;
+    keyword = document.querySelectorAll('#keyword');
 
-//     function myFunction() {
-//         if (window.pageYOffset >= sticky) {
-//             navbar.classList.add("sticky")
-//         } else {
-//             navbar.classList.remove("sticky");
-//         }
-//     }
-//     // document.write("Document loaded successful!");
-// }, 1000);
+    keyword.forEach(e => {
+        e.setAttribute('href', `https://google.com/search?q=${e.innerHTML}`);
+        e.setAttribute('target', '_blank');
+    });
+
+    past_keyword = document.getElementById('past-keyword');
+    for (let id = 0; id < 5; id++) {
+        past_keyword.innerHTML += `${'keyword'+id}; `;
+    }
+
+    project_keyword = document.getElementById('project-keyword');
+    for (let id = 0; id < 5; id++) {
+        project_keyword.innerHTML += `${'keyword'+id}; `;
+    }
+
+    past = document.getElementById('past');
+    past_temp = "";
+    for (let id = 0; id < 5; id++) {
+        past_temp += past.outerHTML;
+    }
+    // console.log(past_temp);
+    past.outerHTML = past_temp;
+}, 1000);
